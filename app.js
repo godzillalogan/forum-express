a4a6efc7ee3c0acd75b05a2bbca185818acf42fe
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-25 01:50:07 +0800

    U97 建立使用者註冊流程

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine','handlebars')
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
