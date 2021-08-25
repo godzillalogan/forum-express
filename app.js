9bee668d4854d2490bebc05a12a69060177708c1
godzillalogan <kenlogan0117@gmail.com>
2021-08-25 15:02:27 +0800

    U98 表單驗證與錯誤訊息

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -8,7 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine','handlebars')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
