129a3efd01b384d2dc4d6f060c714c6d30ee5796
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-27 00:18:57 +0800

    設定Procfile

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const methodOverride = require('method-override')
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
