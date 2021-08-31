56f7269f95003635ae3706479fa4e6a24b0b068f
godzillalogan <kenlogan0117@gmail.com>
2021-08-31 15:37:42 +0800

    把種子資料categories和restaurants順序改變

--- seeders/20210831072142-restaurants-seed-file.js
+++ seeders/20210831072142-restaurants-seed-file.js
@@ -4,8 +4,7 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Restaurants',
-      //因為我想要 50 筆資料，所以先用 Array.from({ length: 50}) 產生長度為 50 的陣列，再用 map 讓每一個元素都對應到一個餐廳的資料
-      Array.from({ length: 50 }).map((d, i) =>
+    Array.from({ length: 50 }).map((d, i) =>
       ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
@@ -17,7 +16,7 @@ module.exports = {
         updatedAt: new Date(),
         CategoryId: Math.floor(Math.random() * 6) * 10 + 1
       })
-      ), {})
+    ), {})
   },
 
   down: async (queryInterface, Sequelize) => {
