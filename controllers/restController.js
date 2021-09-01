b3c018bb12daa6b771b83b38c4bb01f5ff0a0aef
godzillalogan <kenlogan0117@gmail.com>
2021-09-01 15:13:36 +0800

    U136 前台瀏覽餐廳總表

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,20 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    //用 Restaurant.findAll 從 Restaurant model 裡取出資料，並運用 include 一併拿出關聯的 Category model
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.dataValues,  //展開的是第二層 dataValues 裡面的物件
+        description: r.dataValues.description.substring(0, 50), //substring 來截斷文字
+        categoryName: r.Category.name
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 module.exports = restController
