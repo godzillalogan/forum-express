262674f93cf53b7a835585a9d5ad867def3e1a27
godzillalogan <kenlogan0117@gmail.com>
2021-09-01 16:11:31 +0800

    U138 依分類篩選餐廳總表

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,15 +4,31 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
+    const whereQuery = {}  //這是要傳給 findAll 的參數，需要包裝成物件格式
+    let categoryId = ''  //這是要放在 whereQuery 裡的內容
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.CategoryId = categoryId
+    }
     //用 Restaurant.findAll 從 Restaurant model 裡取出資料，並運用 include 一併拿出關聯的 Category model
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    Restaurant.findAll(({ 
+      include: Category,
+      where: whereQuery 
+    })).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,  //展開的是第二層 dataValues 裡面的物件
         description: r.dataValues.description.substring(0, 50), //substring 來截斷文字
         categoryName: r.Category.name
       }))
-      return res.render('restaurants', {
-        restaurants: data
+      Category.findAll({ 
+        raw: true,
+        nest: true
+      }).then(categories => { // 取出 categoies 
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId
+        })
       })
     })
   },
