e16391b2bad984014783054d0de8f9b6307536af
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-09-05 22:37:26 +0800

    A15: (A20) 餐廳資訊整理：Dashboard  Q2

--- controllers/restController.js
+++ controllers/restController.js
@@ -59,6 +59,7 @@ const restController = {
           { model: Comment, include: [User] }
         ]
       }).then(restaurant => {
+        restaurant.increment('viewCounts',{ by:1 })
         return res.render('restaurant', {
           restaurant: restaurant.toJSON() //和 findByPk 搭配時可以用 toJSON()
         })
