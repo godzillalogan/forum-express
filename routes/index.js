8b0d211ebca9ad8aa359ff5d2eeaba628ec7ae8a
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-09-06 22:58:18 +0800

    U153 實作追蹤功能

--- routes/index.js
+++ routes/index.js
@@ -87,4 +87,7 @@ module.exports = (app, passport) => {
   //like
   app.post('/like/:restaurantId', authenticated, userController.addLike)
   app.delete('/like/:restaurantId', authenticated, userController.removeLike)
+  //following
+  app.post('/following/:userId', authenticated, userController.addFollowing)
+  app.delete('/following/:userId', authenticated, userController.removeFollowing)
 }
